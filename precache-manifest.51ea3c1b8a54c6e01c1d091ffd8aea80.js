ad8c733088348e64f27d7e3027292a42d486b33c
Yuki <yukarihirako.13@gmail.com>
2020-09-14 19:08:35 -0300

    Updates

---
+++ precache-manifest.51ea3c1b8a54c6e01c1d091ffd8aea80.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7606d7a989a90612d2b7fa73e7309fc",
+    "url": "/projeto2-web/index.html"
+  },
+  {
+    "revision": "1c874d053b8f283eee58",
+    "url": "/projeto2-web/static/css/main.04070f5d.chunk.css"
+  },
+  {
+    "revision": "033e3c107f23618105e0",
+    "url": "/projeto2-web/static/js/2.eb4e1ab6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projeto2-web/static/js/2.eb4e1ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c874d053b8f283eee58",
+    "url": "/projeto2-web/static/js/main.5c614e75.chunk.js"
+  },
+  {
+    "revision": "a33221a8c9aa49db62b9",
+    "url": "/projeto2-web/static/js/runtime-main.74958662.js"
+  },
+  {
+    "revision": "9645bb2fb1bc431779c178cf1e36dc01",
+    "url": "/projeto2-web/static/media/SP+icon.9645bb2f.svg"
+  },
+  {
+    "revision": "54ce3a3646c6fd429a4e66f87386e4d2",
+    "url": "/projeto2-web/static/media/nike.54ce3a36.svg"
+  },
+  {
+    "revision": "ecef733a33ceec8dc32d29510998afe9",
+    "url": "/projeto2-web/static/media/search.ecef733a.svg"
+  }
+]);
