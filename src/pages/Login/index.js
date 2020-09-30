45d18162e07673aade3bfcf968d94c11b80eb504
Yuki <yukarihirako.13@gmail.com>
2020-09-29 23:39:25 -0300

    Add notification success/failed

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,33 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Form from "../../components/form";
 import TitlePag from "../../components/title";
 import axios from "axios";
 import { Link, useHistory } from "react-router-dom";
-import "../Login/login.css";
+import "../Register/notification.css";
+
+const MESSAGE_TIMEOUT = 2000;
 
 export default function Login() {
     let history = useHistory();
+    const [message, setMessage] = useState({ message: "", type: "" });
 
     async function enviaFormulario(e, email, password) {
         e.preventDefault();
-        const resposta = await axios({
-            method: "post",
-            url: "https://reqres.in/api/login",
-            data: {
-                email,
-                password,
-            },
-        });
-        if (resposta.data) {
-            localStorage.setItem("token", resposta.data.token);
-            history.push("/projeto2-web");
+        try {
+            const resposta = await axios({
+                method: "post",
+                url: "https://reqres.in/api/login",
+                data: {
+                    email,
+                    password,
+                },
+            });
+            if (resposta.data) {
+                setMessage({
+                    message: "Login realizado com sucesso.",
+                    type: "success",
+                });
+                localStorage.setItem("token", resposta.data.token);
+                setTimeout(() => {
+                    history.push("/projeto2-web");
+                }, MESSAGE_TIMEOUT);
+            }
+        } catch (error) {
+            setMessage({
+                message: "Ocorreu um erro ao logar. Tente novamente.",
+                type: "error",
+            });
         }
     }
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setMessage({ message: "", type: "" });
+        }, MESSAGE_TIMEOUT);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [message]);
+
     return (
         <div>
+            <div
+                className={`message ${
+                    message.message ? "visible" : "invisible"
+                } ${message.type}`}
+            >
+                {message.message}
+            </div>
             <TitlePag text="Entrar" />
             <Form buttonText="Login" enviaFormulario={enviaFormulario} />
+
             <div className="link">
                 <Link to="/projeto2-web/register">
                     Não está registrado? Junte-se a nós.
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,26 +1,62 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import Form from "../../components/form";
 import TitlePag from "../../components/title";
+import "./notification.css";
+
+const MESSAGE_TIMEOUT = 2000;
 
 export default function Register() {
     let history = useHistory();
+    const [message, setMessage] = useState({ message: "", type: "" });
 
     async function enviaFormulario(e, email, password) {
         e.preventDefault();
-        const resposta = await axios.post("https://reqres.in/api/register", {
-            email,
-            password,
-        });
-        if (resposta.data) {
-            localStorage.setItem("token", resposta.data.token);
-            history.push("/projeto2-web");
+        try {
+            const resposta = await axios.post(
+                "https://reqres.in/api/register",
+                {
+                    email,
+                    password,
+                }
+            );
+            if (resposta.data) {
+                setMessage({
+                    message: "Registro realizado com sucesso.",
+                    type: "success",
+                });
+                localStorage.setItem("token", resposta.data.token);
+                setTimeout(() => {
+                    history.push("/projeto2-web");
+                }, MESSAGE_TIMEOUT);
+            }
+        } catch (error) {
+            setMessage({
+                message: "Ocorreu um erro ao registrar. Tente novamente.",
+                type: "error",
+            });
         }
     }
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setMessage({ message: "", type: "" });
+        }, MESSAGE_TIMEOUT);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [message]);
+
     return (
         <div>
+            <div
+                className={`message ${
+                    message.message ? "visible" : "invisible"
+                } ${message.type}`}
+            >
+                {message.message}
+            </div>
             <TitlePag text="Inscreva-se" />
             <Form buttonText="Criar Conta" enviaFormulario={enviaFormulario} />
 
