675ebfa4f546472ebf3223c24b72bdbfe6343fc3
Yuki <yukarihirako.13@gmail.com>
2020-11-03 22:56:17 -0300

    Fix private route

--- src/routes/privateRoute.js
+++ src/routes/privateRoute.js
@@ -6,10 +6,12 @@ import axios from "axios";
 function PrivateRoute({ component: Component, ...rest }) {
     const [cookies, setCookie] = useCookies(["token"]);
     const isAuthenticated = async () => {
-        axios.get(
+        const response = await axios.get(
             "https://projeto2-web-backend-wat.herokuapp.com/verifyToken",
             { xsrfCookieName: "token", withCredentials: true }
         );
+        if (response.status === 200) return true;
+        return false;
     };
 
     return (
