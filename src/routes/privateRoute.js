e5de8e9cef98dfffd918d81ef62213ca324f48ab
Yuki <yukarihirako.13@gmail.com>
2020-11-03 23:04:30 -0300

    debug private route

--- src/routes/privateRoute.js
+++ src/routes/privateRoute.js
@@ -6,13 +6,17 @@ import axios from "axios";
 function PrivateRoute({ component: Component, ...rest }) {
     const [cookies, setCookie] = useCookies(["token"]);
     const isAuthenticated = async () => {
-        const response = await axios.get(
-            "https://projeto2-web-backend-wat.herokuapp.com/verifyToken",
-            { xsrfCookieName: "token", withCredentials: true }
-        );
-        console.log(response.request);
-        if (response.status === 200) return true;
-        return false;
+        try {
+            const response = await axios.get(
+                "https://projeto2-web-backend-wat.herokuapp.com/verifyToken",
+                { xsrfCookieName: "token", withCredentials: true }
+            );
+            if (response.status === 200) return true;
+            console.log(response.status);
+        } catch (err) {
+            console.log(err.response);
+            return false;
+        }
     };
 
     return (
